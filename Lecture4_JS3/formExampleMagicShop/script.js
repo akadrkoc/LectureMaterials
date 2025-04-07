b978506d44a7716d743c0cf0e7a80044337cf257
Abdulkadir Koc <abdulkadir1308@gmail.com>
2025-04-07 16:53:25 +0200

    practice

--- Lecture4_JS3/formExampleMagicShop/script.js
+++ Lecture4_JS3/formExampleMagicShop/script.js
@@ -1,25 +1,25 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById('job-type').addEventListener('change', function () {
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('food-type').addEventListener('change', function () {
         const curseStrength = document.getElementById('curse-strength-container');
-        const curseCheckBox = document.getElementById('curse-removal');
-        if (curseCheckBox.checked) {
+        const spicyRadioButton = document.getElementById('curse-removal');
+        if (spicyRadioButton.checked) {
             curseStrength.classList.remove("hidden");
         } else {
             curseStrength.classList.add("hidden");
         }
     });
 
-    // Hijack the form submit action
-    document.getElementById('order-form').addEventListener('submit',
-        function (event) {
-            event.preventDefault(); // Prevent the default form submission behaviour
 
-            const formData = new FormData(event.target);
-            const data = Object.fromEntries(formData.entries());
-            console.log(data);
+    document.getElementById('order-form').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-            // Reset the form
-            event.target.reset();
-            document.getElementById('curse-strength-container').classList.add("hidden");
-        });
-})
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+
+        event.target.reset();
+
+        document.getElementById('curse-strength-container').classList.add("hidden");
+    });
+});
